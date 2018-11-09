3a0e59ccfa7d8ce11a9630120c84a214c8c1ab3e
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-11-09 16:26:27 +0100

    add python 3.7 support

--- youtube-dl-wrapper/Program.cs
+++ youtube-dl-wrapper/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -48,7 +48,21 @@ namespace youtube_dl_wrapper
 
 
             List<Dictionary<string, string>> pythonRegKeys = new List<Dictionary<string, string>>();
-
+			
+            pythonRegKeys.Add(
+                new Dictionary<string, string>() {
+                    { "Path", @"SOFTWARE\Python\PythonCore\3.7-32\InstallPath" },
+                    { "Dir", "HKLM" },
+                    { "Key", "ExecutablePath" },
+                }
+            );
+            pythonRegKeys.Add(
+                new Dictionary<string, string>() {
+                    { "Path", @"SOFTWARE\Python\PythonCore\3.7-32\InstallPath" },
+                    { "Dir", "HKCU" },
+                    { "Key", "ExecutablePath" },
+                }
+            );
             pythonRegKeys.Add(
                 new Dictionary<string, string>() {
                     { "Path", @"SOFTWARE\Python\PythonCore\3.6\InstallPath" },
@@ -80,6 +94,8 @@ namespace youtube_dl_wrapper
 
             string[] pythonPaths = new string[]
             {
+                @"C:\Python37\python.exe",
+                @"C:\Python37-32\python.exe",
                 @"C:\Python36\python.exe",
                 @"C:\Python35\python.exe",
             };
